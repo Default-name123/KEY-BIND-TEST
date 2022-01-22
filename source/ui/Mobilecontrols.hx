0839bc196db51dba667abe17988a60be73b6a62f
MarioMaster2021 <95196453+MarioMaster2021@users.noreply.github.com>
2022-01-21 22:29:10 -0500

    LAST FIX

--- source/ui/Mobilecontrols.hx
+++ source/ui/Mobilecontrols.hx
@@ -3,7 +3,6 @@ package ui;
 import flixel.FlxG;
 import flixel.group.FlxSpriteGroup;
 
-import ui.FlxVirtualPad;
 import ui.Hitbox;
 
 import Config;
@@ -13,7 +12,6 @@ class Mobilecontrols extends FlxSpriteGroup
 	public var mode:ControlsGroup = HITBOX;
 
 	public var _hitbox:Hitbox;
-	public var _virtualPad:FlxVirtualPad;
 
 	var config:Config;
 
@@ -29,12 +27,6 @@ class Mobilecontrols extends FlxSpriteGroup
 
 		switch (mode)
 		{
-			case VIRTUALPAD_RIGHT:
-				initVirtualPad(0);
-			case VIRTUALPAD_LEFT:
-				initVirtualPad(1);
-			case VIRTUALPAD_CUSTOM:
-				initVirtualPad(2);
 			case HITBOX:
 				_hitbox = new Hitbox();
 				add(_hitbox);
@@ -42,43 +34,20 @@ class Mobilecontrols extends FlxSpriteGroup
 		}
 	}
 
-	function initVirtualPad(vpadMode:Int) 
-	{
-		switch (vpadMode)
-		{
-			case 1:
-				_virtualPad = new FlxVirtualPad(FULL, NONE);
-			case 2:
-				_virtualPad = new FlxVirtualPad(FULL, NONE);
-				_virtualPad = config.loadcustom(_virtualPad);
-			default: // 0
-				_virtualPad = new FlxVirtualPad(RIGHT_FULL, NONE);
-		}
-		
-		_virtualPad.alpha = 0.75;
-		add(_virtualPad);	
-	}
-
 
 	public static function getModeFromNumber(modeNum:Int):ControlsGroup {
 		return switch (modeNum)
 		{
-			case 0: VIRTUALPAD_RIGHT;
-			case 1: VIRTUALPAD_LEFT;
-			case 2: KEYBOARD;
-			case 3: VIRTUALPAD_CUSTOM;
-			case 4:	HITBOX;
+			case 0: HITBOX;
+			case 1: KEYBOARD;
 
-			default: VIRTUALPAD_RIGHT;
+			default: HITBOX;
 
 		}
 	}
 }
 
 enum ControlsGroup {
-	VIRTUALPAD_RIGHT;
-	VIRTUALPAD_LEFT;
-	KEYBOARD;
-	VIRTUALPAD_CUSTOM;
 	HITBOX;
+	KEYBOARD;
 }
